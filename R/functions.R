5f72916a4ffa15cf0b93cb2f8c12f3e2a5a8f6c7
Jian Yen <jdl.yen@gmail.com>
2020-01-14 10:35:29 +1100

    add helper functions to clean raw data

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,36 @@
 # define helper functions we need for the analysis
 
+# recode levels to remove typos or confusing health statuses
+recode_levels <- function(x) {
+  x <- x %>% mutate(
+    Health = case_when(
+      Health == "healthy" ~ "Healthy",
+      Health == "Healthly" ~ "Healthy",
+      Health == "H" ~ "Healthy",
+      Health == "H eaten" ~ "Healthy",
+      Health == "hralthy" ~ "Healthy",
+      Health == "Health" ~ "Healthy",
+      Health == "Ununhealthy" ~ "Unhealthy",
+      Health == "unhealthy" ~ "Unhealthy",
+      Health == "poor" ~ "Unhealthy",
+      Health == "Poor" ~ "Unhealthy",
+      Health == "Dea" ~ "Dead",
+      Health == "D" ~ "Dead",
+      Health == "DEAD" ~ "Dead",
+      Health == "Yellow" ~ "NA",
+      Health == "?" ~ "NA",
+      TRUE ~ as.character(Health)
+    )
+  )
+}
+
+# fix up NAs coded as characters
+na_is_logical <- function(x) {
+  x %>% mutate(
+    Health = ifelse(Health == "NA", NA, Health)
+  )
+}
+
 # function to calculate survival curve from fitted parameters
 surv_fn <- function(t, alpha, mu, beta, x) {
   sigma_i <- c(exp(-1 * (mu + beta %*% x) / alpha))
