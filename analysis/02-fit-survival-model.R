934cdcf8dd56964754759b658c2541127590828f
Jian Yen <jdl.yen@gmail.com>
2020-01-14 16:40:16 +1100

    working brm model following changes to makevars

--- analysis/02-fit-survival-model.R
+++ analysis/02-fit-survival-model.R
@@ -5,17 +5,17 @@ library(brms)
 survival_data <- readRDS("data/compiled/survival-data.rds")
 
 # we can use the brms package to fit a survival model
-
-### cens(censored) where censored is "left", "none", "right", "interval" or
-###    -1, 0, 1, 2.
+#    Uses a standard R formula interface but with an additional
+#      term to identify censored observations. This is handled on
+#      the LHS of the formula, separated from days survived by a `|`.
+#      The values of `censored` must take one of "left", "none", "right",
+#      "interval" or, equivalently, -1 (left), 0 (none), 1 (right), 2 (interval).
+#      If interval-censored, then an additional value is needed in `cens()`
+#      to define the interval.
 survival_model <- brm(days | cens(censored) ~ (1 | species) + (1 | site),
                       data = survival_data,
                       family = lognormal())
 
-# the Stan outputs aren't directly setup to make plots
-#    We can reformat them and pull out the parameters we care about most
-#    (alpha, mu, and the `beta_bg` parameters)
-surv_draws <- as.matrix(surv_mod)
-alpha_draws <- surv_draws[, grep("alpha$", colnames(surv_draws))]
-mu_draws <- surv_draws[, grep("mu$", colnames(surv_draws))]
-beta_draws <- surv_draws[, grep("beta_bg\\[", colnames(surv_draws))]
+## Errors in rstan compilation on OSX Catalina:
+##    solve by updating Makevars as suggested at
+##    https://thecoatlessprofessor.com/programming/cpp/r-compiler-tools-for-rcpp-on-macos/
